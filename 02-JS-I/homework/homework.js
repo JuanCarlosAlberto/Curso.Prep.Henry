eff2a56c79a70f07f192acd44589bb54409fadd6
Juan Carlos <juancarlosalbertoestevez@gmeil.com>
2021-12-28 21:18:39 -0300

    modified function obtenerResto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -82,7 +82,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(str1.length === str2.lenth){
+  if(str1.length === str2.length){
     return true
   }else{
     return false
@@ -94,6 +94,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num < 90){
+    return true
+  }else{
+    return false
+  }
   
 }
 
@@ -101,7 +106,11 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num>50){
+    return true
+  }else{
+    return false
+  }
 }
 
 function obtenerResto(x, y) {
