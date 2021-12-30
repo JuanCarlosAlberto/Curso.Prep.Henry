5e473927b347cdc4e4f380466dea7b7faee1fc16
Juan Carlos <juancarlosalbertoestevez@gmeil.com>
2021-12-30 20:34:00 -0300

    Modified function add SinboloExclamacion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -203,6 +203,12 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+ 
+  let str2 = '!';
+  let strT = str + str2 ;
+
+  return strT ; 
+
 }
 
 function combinarNombres(nombre, apellido) {
