8872b1a8121861209722ffad75fcc36de5938845
Juan Carlos <juancarlosalbertoestevez@gmeil.com>
2021-12-30 21:14:36 -0300

    Modified functionObtenerSaludo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -224,7 +224,11 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  let strHola = "Hola";
+  let strVacio = " ";
+  let str2 = "!";
+  let strSaludo = strHola + strVacio + nombre + str2 ;
+  return strSaludo ;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
