fc3028e271f662b469af8f0163dfe3e169d38771
Juan Carlos <juancarlosalbertoestevez@gmeil.com>
2022-01-01 16:43:04 -0300

    modified function deEuroAdolar

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -260,6 +260,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  let dolar= 1.20;
+  let cambio = euro * dolar;
+  return cambio
   
 }
 
